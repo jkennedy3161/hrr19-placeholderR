20e5f1bacf57a3b8a0b5a5679cf7810453813c50
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-24 19:55:36 -0800

    add webpack config for development

---
+++ webpack.dev.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var config = {
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/main.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js',
+    publicPath: '/client/dist/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        loader: 'babel',
+        exclude: path.join(__dirname, 'node_modules') },
+      // { test: /\.scss?$/,
+      //   loader: 'style!css!sass',
+      //   include: path.join(__dirname, 'src', 'styles') },
+      { test: /\.png$/,
+        loader: 'file' },
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file'}
+    ]
+  }
+};
+
+module.exports = config;
